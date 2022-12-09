d4d6aa1b455ac20262b2c5b575cace553486a3c5
alxfady <you@example.com>
2022-12-08 17:44:28 -0800

    5printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- that's too far but w8 vi just auto added *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

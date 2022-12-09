a160e37f4ccb2390bf01d67b2e78c9687adf1ee2
alxfady <you@example.com>
2022-12-08 17:38:17 -0800

    betty sucks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main- that's too far but w8 vi just auto added *
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

232a1b699fe498e618ca11e1089e3c81d136debf
alxfady <you@example.com>
2022-12-08 18:12:25 -0800

    last

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main- that's too far but w8 vi just auto added *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long));
+	printf("Size of a flot: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}

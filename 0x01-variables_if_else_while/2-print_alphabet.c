92a04750d338c2cf38edf90c5286fb653434bce8
alxfady <you@example.com>
2022-12-17 06:48:51 -0800

    2-x

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print lower case alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}

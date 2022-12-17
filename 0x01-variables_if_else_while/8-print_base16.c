3a614de921922a76b9f241ebb3c46b07a0538a40
alxfady <you@example.com>
2022-12-17 07:44:10 -0800

    auto

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print lower case alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

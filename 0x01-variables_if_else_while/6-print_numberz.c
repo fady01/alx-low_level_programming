49d52108b7672e217e5e4d0291008c56a017cfb2
alxfady <you@example.com>
2022-12-17 07:31:22 -0800

    ??

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lower case alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

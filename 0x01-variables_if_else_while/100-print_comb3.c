857324b679c073af2a23c0d219158ad008f57caa
alxfady <you@example.com>
2022-12-17 08:29:35 -0800

    auto

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print number combintations of multible didgit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char j = '1', i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+}

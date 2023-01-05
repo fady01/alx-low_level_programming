791c1d148948764502c85d7b4504e224796fb1d6
alxfady <you@example.com>
2023-01-04 16:44:21 -0800

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,16 @@
 /**
  * _puts_recursion - prints string
- * */
-
+ */
 #include "main.h"
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0'){
+	if (*s != '\0')
+	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
+
 }
